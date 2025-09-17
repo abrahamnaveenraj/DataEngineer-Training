f3220e62707f83375c9bf099b9194809ada45af6
abrahamnaveenraj <2111123@nec.edu.in>
2025-09-17 16:36:53 +0530

    Update Process Employees.py

--- Sept 17th/Process Employees.py
+++ Sept 17th/Process Employees.py
@@ -7,3 +7,9 @@ with open("employees.csv","r") as file:
 print("Employees:")
 for e in employees:
     print(f"{e['id']} - {e['name']} ({e['department']})- {e['salary']}")
+
+salaries=[int(e['salary']) for e in employees]
+totalsal=sum(salaries)
+avgsalary=totalsal/len(salaries)
+print(f"Total Salary: {totalsal}")
+print(f"Average Salary: {avgsalary:.2f}")
